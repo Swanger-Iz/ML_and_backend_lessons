3efe2968d5697b77505a1669795ff76e4265a284
Ilgiz Magadeiv <swanger789@gmail.com>
2026-03-11 20:31:58 +0300

    Learned POST reqs

--- fastapi_course/main.py
+++ fastapi_course/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI, HTTPException
-from schemas import BANDS, Band, GenreURLChoises
+from schemas import BANDS, BandBase, BandCreate, BandWithID, GenreChoises
 
 app = FastAPI()
 
 
-@app.get("/bands", status_code=206, response_model=list[Band])
-async def bands(genre: GenreURLChoises | None = None, has_albums: bool = False) -> list[Band]:
-    band_list = [Band(**b) for b in BANDS]
+@app.get("/bands", status_code=206, response_model=list[BandWithID])
+async def bands(genre: GenreChoises | None = None, has_albums: bool = False) -> list[BandWithID]:
+    band_list = [BandWithID(**b) for b in BANDS]
 
     if genre:
-        band_list = [b for b in band_list if b.genre == genre]
+        band_list = [b for b in band_list if b.genre.lower() == genre]
 
     if has_albums:
         band_list = [b for b in band_list if len(b.albums) > 0]
@@ -22,11 +22,19 @@ async def about() -> str:
     return "Some gachi company"
 
 
-@app.get("/bands/{band_id}", status_code=206, response_model=Band)
-async def get_band_id(band_id: int) -> Band:
-    band = next((Band(**b) for b in BANDS if b["id"] == band_id), None)
+@app.get("/bands/{band_id}", status_code=206, response_model=BandWithID)
+async def get_band_id(band_id: int) -> BandWithID:
+    band = next((BandWithID(**b) for b in BANDS if b["id"] == band_id), None)
     if band is None:
         raise HTTPException(404, "No page found")
+    return band
+
+
+@app.post("/bands", status_code=200, response_model=BandWithID)
+async def create_band(band_data: BandCreate) -> BandWithID:
+    id = BANDS[-1]["id"] + 1
+    band = BandWithID(id=id, **band_data.model_dump()).model_dump()
+    BANDS.append(band)
     return band
 
 
--- fastapi_course/schemas.py
+++ fastapi_course/schemas.py
@@ -1,7 +1,7 @@
 from datetime import date
 from enum import Enum
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
 
 
 class GenreURLChoises(Enum):
@@ -11,17 +11,25 @@ class GenreURLChoises(Enum):
     HIPHOP = "hip-hop"
 
 
+class GenreChoises(Enum):
+    POP_ROCK = "Pop rock"
+    BRITISH_ROCK = "British rock"
+    GLAM_ROCK = "Glam rock"
+    HIPHOP = "Hip-hop"
+    THW = "Some genre with three words"
+
+
 BANDS = [
     {
         "id": 1,
         "name": "Blink 182",
-        "genre": GenreURLChoises.POP_ROCK,
+        "genre": GenreChoises.POP_ROCK,
         "albums": [{"title": "Album number one", "release_date": "2002-07-21"}, {"title": "Album number two", "release_date": "1999-06-17"}],
     },
-    {"id": 2, "name": "Oasis", "genre": GenreURLChoises.BRITISH_ROCK, "albums": []},
-    {"id": 3, "name": "Kiss", "genre": GenreURLChoises.GLAM_ROCK, "albums": []},
-    {"id": 4, "name": "Wu-Tang Clan", "genre": GenreURLChoises.HIPHOP, "albums": []},
-    {"id": 5, "name": "Kino", "genre": GenreURLChoises.POP_ROCK, "albums": []},
+    {"id": 2, "name": "Oasis", "genre": GenreChoises.BRITISH_ROCK, "albums": []},
+    {"id": 3, "name": "Kiss", "genre": GenreChoises.GLAM_ROCK, "albums": []},
+    {"id": 4, "name": "Wu-Tang Clan", "genre": GenreChoises.HIPHOP, "albums": []},
+    {"id": 5, "name": "Kino", "genre": GenreChoises.POP_ROCK, "albums": []},
 ]
 
 
@@ -30,12 +38,24 @@ class Album(BaseModel):
     release_date: date
 
 
-class Band(BaseModel):
-    id: int
+class BandBase(BaseModel):
     name: str
-    genre: GenreURLChoises
+    genre: GenreChoises
     albums: list[Album] = []
 
 
-result = [Band(**b) for b in BANDS if b["genre"] == GenreURLChoises.POP_ROCK and len(b["albums"]) > 0]
+class BandCreate(BandBase):
+    @field_validator("genre", mode="before")
+    def title_case_genre(cls, value):
+        value = value.split(" ")
+        value[0] = value[0].title()
+        value[1:] = [el.lower() for el in value[1:]]
+        return " ".join(value)  # rock/RoCK -> Rock (pop rock -> Pop Rock)
+
+
+class BandWithID(BandBase):
+    id: int
+
+
+result = [BandBase(**b) for b in BANDS if b["genre"] == GenreChoises.POP_ROCK and len(b["albums"]) > 0]
 print(result)
